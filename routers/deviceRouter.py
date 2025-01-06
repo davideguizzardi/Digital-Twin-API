22ce875d16bb87177208d0ca9b87e008e2ede657
davideguizzardi <guizzadavide@gmail.com>
2025-01-06 14:50:42 +0100

    Added call to obtain devices usage data and use json static mapping for unknown devices

--- routers/deviceRouter.py
+++ routers/deviceRouter.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter,HTTPException
 from homeassistant_functions import (
     getDevicesFast,getDevicesNameAndId,
     getSingleDeviceFast)
+from database_functions import (get_all_appliances_usage_entries)
+from collections import defaultdict
+import json
+
 
 def getDeviceRouter():
     device_router=APIRouter(tags=["Device"],prefix="/device")
@@ -21,6 +25,41 @@ def getDeviceRouter():
         if res["status_code"]!=200:
             raise HTTPException(status_code=res["status_code"],detail=res["data"])
         return res["data"]
+    
+
+    @device_router.get("/usage/all")
+    def Get_All_Device_Usage_Data():
+        usage_data=get_all_appliances_usage_entries()
+        devices_list=getDevicesNameAndId()["data"]
+        result=defaultdict(lambda:[])
+        for data in usage_data:
+            device_name=[x for x in devices_list if x["device_id"]==data["device_id"]][0]["name"]
+            result[device_name].append(
+                {
+                    "device_id":data["device_id"],
+                    "state":data["state"],
+                    "average_duration":data["average_duration"],
+                    "average_power":data["average_power"]
+                }
+            )
+
+        #TODO: remove this part in the future, it is used only to test purposed
+        virtual_devices=["washing_machine","microwave"]
+        with open("./data/appliances_consumption_map.json") as file:
+            consumption_map=json.load(file)
+            for device in virtual_devices:
+                for state in consumption_map[device]:
+                    result[device].append(
+                    {
+                        "device_id":f"virtual.{device}",
+                        "state":state["name"],
+                        "average_duration":state.get("default_duration",0)/60,
+                        "average_power":state["power_consumption"]
+                    }
+            )
+
+        return dict(result)
+        
 
     
     return device_router
